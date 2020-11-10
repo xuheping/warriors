aadee35f0c00ffb00a39734c6c7179be7c8ad53f
xuheping <842126343@qq.com>
2020-11-10 10:21:28 +0800

    merge file

--- warriors-houston/src/main/java/com/hp/warriors/WarriorsHoustonApplication.java
+++ warriors-houston/src/main/java/com/hp/warriors/WarriorsHoustonApplication.java
@@ -6,16 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-<<<<<<< HEAD
+
 @EnableHystrix
 @EnableFeignClients
 @EnableEurekaClient
-=======
-@EnableFeignClients
-@EnableEurekaClient
-/** 熔断注解*/
-@EnableHystrix
->>>>>>> 818c0f0fd073bd61598b92b8b6a28b4fd6a29fde
 @SpringBootApplication
 public class WarriorsHoustonApplication {
 
